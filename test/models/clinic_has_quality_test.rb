ef0e9de51f1223df4d3f1b604055f12f631c83dd
AngelMora1999 <eduardo.aemc@gmail.com>
2017-04-29 22:37:05 -0500

    set annotate clinic-quality

--- test/models/clinic_has_quality_test.rb
+++ test/models/clinic_has_quality_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: clinic_has_qualities
+#
+#  id         :integer          not null, primary key
+#  clinic_id  :integer
+#  quality_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ClinicHasQualityTest < ActiveSupport::TestCase
