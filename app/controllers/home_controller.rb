ef7a25811ca9cda5c291f58a5333f4d0a84761d4
AngelMora1999 <eduardo.aemc@gmail.com>
2017-04-30 10:59:21 -0500

    complete the scope in devise for all models very complex

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController	
+  def main
+  end
+
+  def index_doctor
+  end
+
+  def index_patient
+  end
+
+  def dashboard
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,38 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :doctors
   devise_for :patients
-  
+
+  #Configuración de los authenticated & unauthenticated in devise
+  #Para los usuarios
+  authenticated :user do
+    root "home#dashboard"
+  end
+
+  #Para los doctores
+  authenticated :doctor do
+    root "home#index_doctor"
+  end
+
+  #Para los pacientes
+  authenticated :patient do
+    root "home#index_patient"
+  end
+
+  #Para los usuarios
+  unauthenticated :user do
+    root "home#main"
+  end
+
+  #Para los doctores
+  unauthenticated :doctor do
+    root "home#main"
+  end
+
+  #Para los pacientes
+  unauthenticated :patient do
+    root "home#main"
+  end
+
   #Modelos
   resources :clinics
   resources :consulting_rooms
