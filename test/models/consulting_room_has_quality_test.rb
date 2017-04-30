2f3b536aeb5087e5a32bb1e72121e1cccd3814eb
AngelMora1999 <eduardo.aemc@gmail.com>
2017-04-29 22:46:18 -0500

    set annotate consulting_room-quality

--- test/models/consulting_room_has_quality_test.rb
+++ test/models/consulting_room_has_quality_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: consulting_room_has_qualities
+#
+#  id                 :integer          not null, primary key
+#  consulting_room_id :integer
+#  quality_id         :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 require 'test_helper'
 
 class ConsultingRoomHasQualityTest < ActiveSupport::TestCase
