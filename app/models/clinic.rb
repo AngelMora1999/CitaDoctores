6f5e0b18744b7f18d69118812608f39a6e38c700
AngelMora1999 <eduardo.aemc@gmail.com>
2017-04-29 20:23:22 -0500

    set annotate in three models

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: clinics
+#
+#  id                :integer          not null, primary key
+#  logo_file_name    :string
+#  logo_content_type :string
+#  logo_file_size    :integer
+#  logo_updated_at   :datetime
+#  name              :string
+#  description       :text
+#  founded_since     :datetime
+#  state             :string
+#  city              :string
+#  address           :text
+#  visit_count       :integer          default("0")
+#  user_id           :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 class Clinic < ApplicationRecord
 end
--- test/models/consulting_room_test.rb
+++ test/models/consulting_room_test.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: consulting_rooms
+#
+#  id                :integer          not null, primary key
+#  logo_file_name    :string
+#  logo_content_type :string
+#  logo_file_size    :integer
+#  logo_updated_at   :datetime
+#  name              :string
+#  description       :text
+#  founded_since     :datetime
+#  state             :string
+#  city              :string
+#  address           :text
+#  visit_count       :integer          default("0")
+#  user_id           :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 require 'test_helper'
 
 class ConsultingRoomTest < ActiveSupport::TestCase
