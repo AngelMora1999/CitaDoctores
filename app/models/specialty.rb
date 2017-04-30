1310547e271d3b9ce07ad198730c29149fd590a5
AngelMora1999 <eduardo.aemc@gmail.com>
2017-04-29 22:03:43 -0500

    set annotate in model specialty

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: specialties
+#
+#  id                 :integer          not null, primary key
+#  title              :string
+#  description        :text
+#  logo_file_name     :string
+#  logo_content_type  :string
+#  logo_file_size     :integer
+#  logo_updated_at    :datetime
+#  cover_file_name    :string
+#  cover_content_type :string
+#  cover_file_size    :integer
+#  cover_updated_at   :datetime
+#  icon               :text
+#  color              :string
+#  visit_count        :integer          default("0")
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 class Specialty < ApplicationRecord
 end
--- test/models/specialty_test.rb
+++ test/models/specialty_test.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: specialties
+#
+#  id                 :integer          not null, primary key
+#  title              :string
+#  description        :text
+#  logo_file_name     :string
+#  logo_content_type  :string
+#  logo_file_size     :integer
+#  logo_updated_at    :datetime
+#  cover_file_name    :string
+#  cover_content_type :string
+#  cover_file_size    :integer
+#  cover_updated_at   :datetime
+#  icon               :text
+#  color              :string
+#  visit_count        :integer          default("0")
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 require 'test_helper'
 
 class SpecialtyTest < ActiveSupport::TestCase
