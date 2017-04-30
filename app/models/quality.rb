e7ac9007a6bd5738e17bab49224432212e463d65
AngelMora1999 <eduardo.aemc@gmail.com>
2017-04-29 22:07:12 -0500

    set annotate in model qualty

--- app/models/quality.rb
+++ app/models/quality.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: qualities
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  icon        :text
+#  color       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Quality < ApplicationRecord
 end
--- test/models/quality_test.rb
+++ test/models/quality_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: qualities
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  icon        :text
+#  color       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class QualityTest < ActiveSupport::TestCase
