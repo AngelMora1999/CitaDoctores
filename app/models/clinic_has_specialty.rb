e9b2ac251dc485524b4197e5c3f5725b5eeb6897
AngelMora1999 <eduardo.aemc@gmail.com>
2017-04-29 22:27:29 -0500

    set annotate clinic-specialty

--- app/models/clinic_has_specialty.rb
+++ app/models/clinic_has_specialty.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: clinic_has_specialties
+#
+#  id           :integer          not null, primary key
+#  clinic_id    :integer
+#  specialty_id :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class ClinicHasSpecialty < ApplicationRecord
   belongs_to :clinic
   belongs_to :specialty
